b2b5220891bd30938259dc6ce642f77f3932efd7
Umar Azam <umarazam497@gmail.com>
2024-08-30 01:58:42 +0500

    Implemented promises

--- src/controllers/titleController.js
+++ src/controllers/titleController.js
@@ -1,5 +1,6 @@
 const callbackService = require("../services/callbackService");
 const flowService = require("../services/flowService");
+const promiseService = require("../services/promiseService");
 
 exports.handleGetTitle = (req, res, implementation) => {
   // To be implemented based on the control flow method
@@ -35,7 +36,22 @@ exports.handleGetTitle = (req, res, implementation) => {
       });
       break;
     case "promises":
-      // Call promiseService.getTitles with appropriate handler
+      promiseService
+        .getTitles(req.query.address)
+        .then((titles) => {
+          res.send(`
+                        <html>
+                        <head></head>
+                        <body>
+                        <h1>Following are the titles of given websites:</h1>
+                        <ul>${titles}</ul>
+                        </body>
+                        </html>
+                    `);
+        })
+        .catch((err) => {
+          res.status(500).send("<h1>Server Error</h1>");
+        });
       break;
     case "streams":
       // Call streamService.getTitles with appropriate handler
---
+++ src/services/promiseService.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const { parse } = require("node-html-parser");
+const { addProtocol } = require("../utils/helpers");
+
+const getTitles = (addresses) => {
+  if (!Array.isArray(addresses)) {
+    addresses = [addresses];
+  }
+
+  const promises = addresses.map((address) => {
+    const url = addProtocol(address);
+    return axios
+      .get(url)
+      .then((response) => {
+        const root = parse(response.data);
+        const title = root.querySelector("title").innerText || "NO RESPONSE";
+        return `<li>${address} - "${title}"</li>`;
+      })
+      .catch(() => {
+        return `<li>${address} - NO RESPONSE</li>`;
+      });
+  });
+
+  return Promise.all(promises).then((results) => results.join(""));
+};
+
+module.exports = { getTitles };
